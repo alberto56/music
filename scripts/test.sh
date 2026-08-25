7786200f5373d6b2a4decccb9c6df05fb5124f91
Albert Albala <albert@mediatribe.net>
2026-08-25 10:43:20 -0400

    Add a list of components (#4)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+set -e
 ./scripts/broken-links.sh
 ./scripts/end-to-end-tests.sh
+./scripts/no-tabs.sh
+./scripts/validate-html.sh
 cat ./ascii-art/ship/ship.txt
 echo "All tests passed successfully!"
