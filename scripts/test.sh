596bad6e7c1e946b9e57bcfd1e2e7dc03b8fd624
Albert Albala <albert@mediatribe.net>
2026-08-25 08:43:38 -0400

    Add all tests to the test.sh script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 ./scripts/broken-links.sh
 ./scripts/end-to-end-tests.sh
+./scripts/no-tabs.sh
+./scripts/validate-html.sh
 cat ./ascii-art/ship/ship.txt
 echo "All tests passed successfully!"
